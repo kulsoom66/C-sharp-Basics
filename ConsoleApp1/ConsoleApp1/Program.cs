c9c2dac76d98ea52a9cfb18dd1bd01da99cbbad5
unknown <shrooqalsaadi2244@gmail.com>
2023-11-14 12:19:28 +0400

    created a hello app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // initailizing variables
+            int age = 23;
+            string name = "Kulthoom";
+            string jobTitle = "Trainer Assistant";
+
+            Console.WriteLine("Hello my name is "+name+", I am "+age+" years old, and I am working in code academy as "+jobTitle);
+        }
+    }
+}
