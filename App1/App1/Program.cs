f8a7f3cd0a0fa4e65460c1663d96fa1d35edd4b4
kulsoom66 <kulsoomshoukat66@gmail.com>
2023-11-14 13:30:34 +0400

    data type

--- App1/App1/Program.cs
+++ App1/App1/Program.cs
@@ -5,10 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-
             string name = "Kulthoom";
             int age = 23;
-            Console.WriteLine(name + " " + age);
+            string job = "Trainer Assistant";
+            Console.WriteLine("It is " + name + ", I am " + age + "years old, and currently working as "+job);
+
+            //Casting
+            float money = 3.45f;
+
+            //implicit casting, from smaller size data type to lager size of the data type
+            int num = 5;
+            long ssn = num;
+            Console.WriteLine(ssn);
+
+            //explicit casting, from lager size data type to smaller size of the data type
+            double payment = 55.3;
+            int realPayment = int(payment);
 
         }
     }
