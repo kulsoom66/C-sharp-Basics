56fe9fcac176eb9ae079c1bff624b21671f02fcb
unknown <shrooqalsaadi2244@gmail.com>
2023-11-14 12:45:29 +0400

    created an app

---
+++ App1/App1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Kulthoom";
+            int age = 23;
+            Console.WriteLine(name + " " + age);
+
+        }
+    }
+}
