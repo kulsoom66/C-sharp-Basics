d0cb92a3b3783c03fa990fb6747dc7601f8f88bd
kulsoom66 <kulsoomshoukat66@gmail.com>
2023-11-16 11:32:39 +0400

    Day3 arrays

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,148 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Casting
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            //n = int.Parse(Console.ReadLine());
+
+            //declaring and initializing array
+            int[] arr = new int[n];
+
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[2] = 3;
+            arr[3] = 4;
+            arr[4] = 5;
+            arr[5] = 6;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
